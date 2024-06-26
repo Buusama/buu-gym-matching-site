5b4cbcfb11f33d270a5a73cedb078b00abd75438
Buusama <Buusama1144@gmail.com>
2024-06-26 23:47:19 +0700

    Refactor file paths for emojis and user icons

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars": ["error", { "varsIgnorePattern": "^React$" }]
+    "@typescript-eslint/no-unused-vars": "off"
   },
 }
