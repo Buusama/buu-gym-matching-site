e78f7cc3f4fcdfca318c522e11fecd2e8950a459
Buusama <Buusama1144@gmail.com>
2024-06-27 01:24:32 +0700

    Refactor import paths for RootState in state slices

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' 
+import App from './App.tsx' 
 //
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
