93636807bf69a632182e18d5865ef2eca803b935
Buusama <Buusama1144@gmail.com>
2024-07-04 16:46:35 +0700

    chore: Update npm dependencies and add Vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {enabled: true},
+      manifest:{
+        name: "Buugym App",
+        short_name: "Buugym",
+        theme_color: "#ffffff",
+        description: "Buugym App",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      }
+    
+    })
+  ],
   define: {
     global: "window",
     "process.env": {},
